c5be7e6f128713df988d23cdb8b0a2d94d6378e0
Stan Swidwinski <stanislaw.swidwinski@yale.edu>
2016-05-03 00:34:19 -0400

    Rebase on master

--- com/onion/OnionMessage.java
+++ com/onion/OnionMessage.java
@@ -1,6 +1,7 @@
 package com.onion;
 
 import java.io.*;
+import java.net.*;
 
 public class OnionMessage implements Serializable {
     public OnionMessage(MsgType type, byte[] data) {
@@ -8,20 +9,11 @@ public class OnionMessage implements Serializable {
 	this.type = type;
     }
 
-    public static enum MsgType {
-	KEY_REQUEST,
-	KEY_REPLY,
-	HOP_REQUEST,
-	HOP_REPLY,
-	POISON,
-	DATA,
-    }
-
-    MsgType type;
+    public int connectionId;
     byte[] data;
 
-    public MsgType getType() {
-	return type;
+    public int getConnectionId() {
+	return connectionId;
 	    
     }
 
@@ -29,7 +21,30 @@ public class OnionMessage implements Serializable {
 	return data;
     }
 
+    // Format: length of msg connectionID MsgType DATA
+    // MsgType is serialized as the ordinal value
     public byte[] pack() {
-	return null;
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        try {
+            baos.write(CipherUtils.serialize(new Integer(data.length)));
+            baos.write(CipherUtils.serialize(new Integer(this.type.ordinal())));
+            baos.write(data, 0, data.length);
+            return baos.toByteArray();
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    public static OnionMessage unpack(Socket sck) {
+        try {
+            ObjectInputStream ois = new ObjectInputStream(sck.getInputStream());
+            Integer length = (Integer) ois.readObject();
+            MsgType type = OnionMessage.MsgType.values()[(Integer) ois.readObject()];
+            byte[] data = new byte[length];
+            ois.read(data, 0, length);
+            return new OnionMessage(type, data);    
+        } catch (Exception e) {
+            return null;
+        }
     }
 }
